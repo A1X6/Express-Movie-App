a98e40de14af333fb0bb4b00ca24572d1558756a
A1X6 <ax.ahmed.ax@gmail.com>
2024-06-01 16:08:41 +0300

    addeded validation for add movies

--- routes/movies.js
+++ routes/movies.js
@@ -1,14 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const moviesController = require('../controllers/moviesController');
-const { validate } = require('../validations/movie.validation');
-const { movieSchema } = require('../validations/movie.validationScheme');
+const moviesController = require("../controllers/moviesController");
+const { validate } = require("../validations/movie.validation");
+const { movieSchema } = require("../validations/movie.validationScheme");
 
-
-
-router.get('/', moviesController.getAllMovies);
-router.post('/', moviesController.addMovie);
-router.put('/:id', validate(movieSchema), moviesController.updateMovie);
+router.get("/", moviesController.getAllMovies);
+router.post("/", validate(movieSchema), moviesController.addMovie);
+router.put("/:id", validate(movieSchema), moviesController.updateMovie);
 // router.delete('/:id', moviesController.deleteMovie);
 
 module.exports = router;
